e2a5f5d9b69446d3896bd8f37fd7bd145990dfae
saladmaki <milk.choco.1147@gmail.com>
2025-08-12 21:33:15 +0900

    commit-2

--- script.js
+++ script.js
@@ -1,64 +1,81 @@
-const pointView = document.getElementById("pointView");
-const levelView = document.getElementById("levelView");
-const LEVEL_Cost = document.getElementById("LEVEL_Cost");
-const AUTO_Cost = document.getElementById("AUTO_Cost");
-const addPointBtn = document.getElementById("addPointBtn");
-const upgradeLevelBtn = document.getElementById("upgradeLevelBtn");
-const upgradeAutoBtn = document.getElementById("upgradeAutoBtn");
+const ui = {
+  pointView: document.getElementById("pointView"),
+  levelView: document.getElementById("levelView"),
+  autoView: document.getElementById("autoView"),
+  LEVEL_Cost: document.getElementById("LEVEL_Cost"),
+  AUTO_Cost: document.getElementById("AUTO_Cost"),
+  addPointBtn: document.getElementById("addPointBtn"),
+  upgradeLevelBtn: document.getElementById("upgradeLevelBtn"),
+  upgradeAutoBtn: document.getElementById("upgradeAutoBtn"),
+};
 
-let POINT = 0;
-let LEVEL = 1;
-let VOLUME = 1;
-let COST = 10;
-let AUTO = 0;
-let AUTO_COST = 2;
+const Status = {
+  point: 0,
+  level: 1,
+  auto: 1,
+  isAuto: false,
+  levelCost: 10,
+  autoCost: 10,
+  autoInterval: 700,
+};
 
-updateStatus();
+updateUi();
 
-addPointBtn.addEventListener("click", () => {
-  POINT += VOLUME;
-  updateStatus();
+// Update Status when click button
+ui.addPointBtn.addEventListener("click", () => {
+  Status.point += Status.level;
+  updateUi();
 });
 
-upgradeLevelBtn.addEventListener("click", () => {
-  if (POINT >= COST) {
-    POINT -= COST;
-    LEVEL++;
-    VOLUME *= 2;
-    COST *= 3;
-    levelView.textContent = LEVEL;
-    LEVEL_Cost.textContent = COST - POINT;
-    
-    updateStatus();
+ui.upgradeLevelBtn.addEventListener("click", () => {
+  if (Status.point >= Status.levelCost) {
+    Status.point -= Status.levelCost;
+    Status.level++;
+    Status.levelCost = (Status.level + 1) * 12;
+    updateUi();
   }
 });
 
-upgradeAutoBtn.addEventListener("click", () => {
-  if (POINT >= COST / 2) {
-    POINT -= COST / 2;
-    AUTO++;
-    AUTO_COST *= 2;
-    updateStatus();
+ui.upgradeAutoBtn.addEventListener("click", () => {
+  if (Status.point >= Status.autoCost) {
+    if (!Status.isAuto) {
+      Status.isAuto = true;
+    }
+    Status.point -= Status.autoCost;
+    Status.autoCost = (Status.auto + 1) * 11;
+    Status.auto++;
+    updateUi();
   }
 });
 
-function updateStatus() {
-  pointView.textContent = POINT;
-  LEVEL_Cost.textContent = COST;
-  AUTO_Cost.textContent = AUTO_COST;
-  if (POINT >= COST) {
-    upgradeLevelBtn.classList.remove("invalid");
+// Automation logic
+setInterval(() => {
+  if (Status.isAuto) {
+    Status.point += Status.auto - 1;
+    updateUi();
+  }
+}, Status.autoInterval);
+
+// Update UI
+function updateUi() {
+  ui.pointView.textContent = Status.point;
+  ui.levelView.textContent = Status.level;
+  ui.autoView.textContent = Status.auto;
+
+  ui.LEVEL_Cost.textContent = Status.levelCost;
+  ui.AUTO_Cost.textContent = Status.autoCost;
+
+  // upgrade level button
+  if (Status.point >= Status.levelCost) {
+    ui.upgradeLevelBtn.classList.remove("invalid");
   } else {
-    upgradeLevelBtn.classList.add("invalid");
+    ui.upgradeLevelBtn.classList.add("invalid");
   }
-  if (POINT >= AUTO_COST) {
-    upgradeAutoBtn.classList.remove("invalid");
+
+  // upgrade automation button
+  if (Status.point >= Status.autoCost) {
+    ui.upgradeAutoBtn.classList.remove("invalid");
   } else {
-    upgradeAutoBtn.classList.add("invalid");
+    ui.upgradeAutoBtn.classList.add("invalid");
   }
 }
-
-setInterval(() => {
-  POINT += AUTO;
-  updateStatus();
-}, 500);
